09127ec5af6a9c4f75e024c224b711af3808140f
Mihail <sadovni4ek@gmail.com>
2024-09-16 20:19:24 +0300

    library deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header-component/header.component";
 import { SearchComponent } from "./components/search/search.component";
+import { MainComponent } from "./components/main/main.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, SearchComponent],
+  imports: [RouterOutlet, HeaderComponent, SearchComponent, MainComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './main.component.scss'
 })
 export class MainComponent {
-
+   
 }
